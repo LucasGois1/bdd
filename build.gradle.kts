0fe5e3ad503d087267ea386ed741c13d39e56c52
Lucas Gois <lucassampaiodegois1@gmail.com>
2023-10-29 20:02:51 -0300

    chore: rollback java version

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ group = "com.contract.producer"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_20
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
